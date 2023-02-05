4ee5418979cf1db853e921fccc78d9111f5f340e
Alessandro Stefani <alessandro.bave@gmail.com>
2023-02-05 22:15:14 +0100

    Add project files.

---
+++ SerialMonitor/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace SerialMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btn_Connect = new System.Windows.Forms.Button();
+            this.AvailablePortsBox = new System.Windows.Forms.ComboBox();
+            this.LBL = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LBL_155_g = new System.Windows.Forms.Label();
+            this.LBL_155_a = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LBL_OMS_g = new System.Windows.Forms.Label();
+            this.LBL_OMS_a = new System.Windows.Forms.Label();
+            this.LBL_status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Serial port:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 186);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(449, 372);
+            this.textBox2.TabIndex = 2;
+            // 
+            // btn_Connect
+            // 
+            this.btn_Connect.Location = new System.Drawing.Point(191, 16);
+            this.btn_Connect.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Connect.Name = "btn_Connect";
+            this.btn_Connect.Size = new System.Drawing.Size(91, 21);
+            this.btn_Connect.TabIndex = 3;
+            this.btn_Connect.Text = "Connect";
+            this.btn_Connect.UseVisualStyleBackColor = true;
+            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
+            // 
+            // AvailablePortsBox
+            // 
+            this.AvailablePortsBox.FormattingEnabled = true;
+            this.AvailablePortsBox.Location = new System.Drawing.Point(75, 15);
+            this.AvailablePortsBox.Name = "AvailablePortsBox";
+            this.AvailablePortsBox.Size = new System.Drawing.Size(111, 21);
+            this.AvailablePortsBox.TabIndex = 4;
+            // 
+            // LBL
+            // 
+            this.LBL.AutoSize = true;
+            this.LBL.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL.Location = new System.Drawing.Point(20, 78);
+            this.LBL.Name = "LBL";
+            this.LBL.Size = new System.Drawing.Size(242, 63);
+            this.LBL.TabIndex = 1;
+            this.LBL.Text = "--- μg/mc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(273, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "D.Lgs 13/08/2010 n° 155:";
+            // 
+            // LBL_155_g
+            // 
+            this.LBL_155_g.AutoSize = true;
+            this.LBL_155_g.Location = new System.Drawing.Point(273, 91);
+            this.LBL_155_g.Name = "LBL_155_g";
+            this.LBL_155_g.Size = new System.Drawing.Size(181, 13);
+            this.LBL_155_g.TabIndex = 1;
+            this.LBL_155_g.Text = "massimo medio giornaliero: 25 μg/mc";
+            // 
+            // LBL_155_a
+            // 
+            this.LBL_155_a.AutoSize = true;
+            this.LBL_155_a.Location = new System.Drawing.Point(273, 78);
+            this.LBL_155_a.Name = "LBL_155_a";
+            this.LBL_155_a.Size = new System.Drawing.Size(189, 13);
+            this.LBL_155_a.TabIndex = 1;
+            this.LBL_155_a.Text = "limite massimo medio annuo: 20 μg/mc";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(273, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "OMS:";
+            // 
+            // LBL_OMS_g
+            // 
+            this.LBL_OMS_g.AutoSize = true;
+            this.LBL_OMS_g.Location = new System.Drawing.Point(273, 140);
+            this.LBL_OMS_g.Name = "LBL_OMS_g";
+            this.LBL_OMS_g.Size = new System.Drawing.Size(181, 13);
+            this.LBL_OMS_g.TabIndex = 1;
+            this.LBL_OMS_g.Text = "massimo medio giornaliero: 15 μg/mc";
+            // 
+            // LBL_OMS_a
+            // 
+            this.LBL_OMS_a.AutoSize = true;
+            this.LBL_OMS_a.Location = new System.Drawing.Point(273, 127);
+            this.LBL_OMS_a.Name = "LBL_OMS_a";
+            this.LBL_OMS_a.Size = new System.Drawing.Size(157, 13);
+            this.LBL_OMS_a.TabIndex = 1;
+            this.LBL_OMS_a.Text = "massimo medio annuo: 5 μg/mc";
+            // 
+            // LBL_status
+            // 
+            this.LBL_status.AutoSize = true;
+            this.LBL_status.Location = new System.Drawing.Point(304, 21);
+            this.LBL_status.Name = "LBL_status";
+            this.LBL_status.Size = new System.Drawing.Size(73, 13);
+            this.LBL_status.TabIndex = 1;
+            this.LBL_status.Text = "Disconnected";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 570);
+            this.Controls.Add(this.AvailablePortsBox);
+            this.Controls.Add(this.btn_Connect);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.LBL);
+            this.Controls.Add(this.LBL_OMS_a);
+            this.Controls.Add(this.LBL_OMS_g);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.LBL_155_a);
+            this.Controls.Add(this.LBL_155_g);
+            this.Controls.Add(this.LBL_status);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Bave VINDRIKTNING ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn_Connect;
+        private System.Windows.Forms.ComboBox AvailablePortsBox;
+        private System.Windows.Forms.Label LBL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LBL_155_g;
+        private System.Windows.Forms.Label LBL_155_a;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label LBL_OMS_g;
+        private System.Windows.Forms.Label LBL_OMS_a;
+        private System.Windows.Forms.Label LBL_status;
+    }
+}
+
---
+++ SerialMonitor/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace SerialMonitor
+{
+    public partial class Form1 : Form
+    {
+
+        // Create the serial port with basic settings 
+        private string ReceivedData; //received data
+        private SerialPort SerPort = new SerialPort();
+        string out_text;
+        string value;
+
+
+        public Form1()
+        {
+        InitializeComponent();
+        FetchAvailablePorts();
+        }
+
+        void FetchAvailablePorts()
+        {
+            String[] ports = SerialPort.GetPortNames();//Put the name of the available USB ports into the ports string array
+            AvailablePortsBox.Items.AddRange(ports); //Add the string into the dropdown list we made
+        }
+
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            LBL_status.Text = "Connecting...";
+
+            if (SerPort.IsOpen)
+            {
+                SerPort.Close();
+                Console.WriteLine("DISCONNECTED");
+                LBL_status.Text = "DISCONNECTED";
+            }
+
+            else
+            {
+                Console.WriteLine("Connection.... Incoming Data:");
+                // Begin communications 
+                SerPort.BaudRate = 9600;
+                SerPort.PortName = AvailablePortsBox.Text;
+                SerPort.DataReceived += SerPort_DataReceived;
+                
+                try
+                {
+                    SerPort.Open(); //We open the port
+                    Thread.Sleep(1000); //We wait a sec
+                    LBL_status.Text = "CONNECTED";
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error!");
+                    LBL_status.Text = "ERROR";
+
+                }
+            }
+        }
+
+        private void SerPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Thread.Sleep(100);
+            int len = SerPort.BytesToRead;
+            byte[] buf = new byte[len];
+            SerPort.Read(buf, 0, len);
+
+            string inbuf = BitConverter.ToString(buf).Replace("-", "");
+
+            if (inbuf.StartsWith("16"))
+            {
+                out_text = inbuf+"\r\n";
+                value = int.Parse(inbuf.Substring(10, 4), System.Globalization.NumberStyles.HexNumber).ToString();
+                this.Invoke(new Action(ProcessingData));
+            }
+
+
+            //}
+
+            
+            //Console.WriteLine("Received Data:" + BitConverter.ToString(buf).Replace("-", ""));
+            //textdata += BitConverter.ToString(buf);
+
+            //ReceivedData = SerPort.ReadByte().ToString("X2"); //We read the serial port
+            //this.Invoke(new Action(ProcessingData)); //execute the delegate (ProcessingData)
+
+        }
+
+        private void ProcessingData()
+        {
+            DateTimeOffset now = (DateTimeOffset)DateTime.UtcNow;
+            textBox2.AppendText(now.ToString("yyyy/MM/dd    HH-mm-ss-fff") + "    " + out_text);
+
+            Color red = System.Drawing.Color.Red;
+            Color green = System.Drawing.Color.Green;
+
+            if (int.Parse(value) > 25)
+            {
+                LBL.ForeColor = red;
+                LBL_155_a.ForeColor = red;
+                LBL_155_g.ForeColor = red;
+                LBL_OMS_a.ForeColor = red;
+                LBL_OMS_g.ForeColor = red;
+            }
+
+            if (int.Parse(value) < 25)
+            {
+                LBL.ForeColor = green;
+                LBL_155_a.ForeColor = red;
+                LBL_155_g.ForeColor = green;
+                LBL_OMS_a.ForeColor = red;
+                LBL_OMS_g.ForeColor = red;
+            }
+
+            if (int.Parse(value) < 20)
+            {
+                LBL.ForeColor = green;
+                LBL_155_a.ForeColor = green;
+                LBL_155_g.ForeColor = green;
+                LBL_OMS_a.ForeColor = red;
+                LBL_OMS_g.ForeColor = red;
+            }
+
+            if (int.Parse(value) < 15)
+            {
+                LBL.ForeColor = green;
+                LBL_155_a.ForeColor = green;
+                LBL_155_g.ForeColor = green;
+                LBL_OMS_a.ForeColor = red;
+                LBL_OMS_g.ForeColor = green;
+            }
+
+            if (int.Parse(value) < 5)
+            {
+                LBL.ForeColor = green;
+                LBL_155_a.ForeColor = green;
+                LBL_155_g.ForeColor = green;
+                LBL_OMS_a.ForeColor = green;
+                LBL_OMS_g.ForeColor = green;
+            }
+
+            LBL.Text = value + " μg/mc";
+        }
+
+        private void prescription_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+            
+    }
+
